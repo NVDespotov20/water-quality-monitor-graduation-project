db2430b01d13f0e091125aa26febbee58c871e1f
NVDespotov20 <nvdespotov20@codingburgas.bg>
2024-11-26 15:17:59 +0200

    Add initial file structure

---
+++ src/FlowGuardMonitoring/FlowGuardMonitoring.DAL/Data/FlowGuardMonitoringContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlowGuardMonitoring.DAL.Data;
+
+public class FlowGuardMonitoringContext : DbContext
+{
+    public FlowGuardMonitoringContext(DbContextOptions<FlowGuardMonitoringContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
